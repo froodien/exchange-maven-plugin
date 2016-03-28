89618b3890970641074bbca68868565d46cec3b6
Pablo Mantellini <pmantellini@mulesoft.com>
2016-03-28 16:57:17 -0300

    Finished Exchange API implementation

--- src/main/java/org/mule/tools/maven/exchange/ExchangeApi.java
+++ src/main/java/org/mule/tools/maven/exchange/ExchangeApi.java
@@ -1,5 +1,7 @@
 package org.mule.tools.maven.exchange;
 
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.lang3.reflect.FieldUtils;
 import org.apache.maven.plugin.logging.Log;
 import org.mule.tools.maven.exchange.api.ExchangeObject;
@@ -9,11 +11,12 @@ import org.mule.tools.maven.plugin.mule.ApiException;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.Invocation;
 import javax.ws.rs.core.Response;
+import java.io.IOException;
 
 public class ExchangeApi extends AbstractMuleApi {
     private static final String EXCHANGE_TOKEN_HEADER = "x-token";
     private static final String EXCHANGE_TOKEN_PATH = "/exchange/api/exchangeToken";
-    // private static final String EXCHANGE_OBJECTS_PATH_TEMPLATE = "/exchange/api/%s/objects";
+    private static final String EXCHANGE_OBJECTS_PATH_TEMPLATE = "/exchange/api/organizations/%s/objects";
     private static final String EXCHANGE_TOKEN_REQUEST_TEMPLATE = "{" +
             "  \"access_token\": \"%s\"" +
             "  }";
@@ -36,25 +39,77 @@ public class ExchangeApi extends AbstractMuleApi {
         }
     }
 
-    public void updateExchangeObject(ExchangeObject exchangeObject) {
-        // TODO Implement with next Exchange release
+    /**
+     * Updates an object entry in Exchange from its model object.
+     * @param exchangeObject The object to create in Exchange.
+     * @return The object when created.
+     * @throws org.mule.tools.maven.plugin.mule.ApiException if not successful.
+     * @throws java.io.IOException if model error
+     */
+    public ExchangeObject updateExchangeObject(ExchangeObject exchangeObject) throws ApiException,IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        Entity<String> json = Entity.json(mapper.writeValueAsString(exchangeObject));
+        String nameUrl = exchangeObject.getNameUrl();
+        String object_path = String.format(EXCHANGE_OBJECTS_PATH_TEMPLATE, getOrgId()) + '/' + nameUrl;
+        Response response = put(uri, object_path, json);
+
+        if (response.getStatus() == 200)
+        {
+            return response.readEntity(ExchangeObject.class);
+        }
+        else
+        {
+            throw new ApiException(response);
+        }
     }
 
-    public void createExchangeObject(ExchangeObject exchangeObject) {
-        // TODO Implement with next Exchange release
+    /**
+     * Creates an object entry in Exchange from its model object.
+     * @param exchangeObject The object to create in Exchange.
+     * @return The object when created.
+     * @throws org.mule.tools.maven.plugin.mule.ApiException if not successful.
+     * @throws java.io.IOException if model error
+     */
+    public ExchangeObject createExchangeObject(ExchangeObject exchangeObject) throws IOException,ApiException {
+        ObjectMapper mapper = new ObjectMapper();
+        Entity<String> json = Entity.json(mapper.writeValueAsString(exchangeObject));
+        String object_path = String.format(EXCHANGE_OBJECTS_PATH_TEMPLATE, getOrgId()) + '/';
+        Response response = post(uri, object_path, json);
+
+        if (response.getStatus() == 200)
+        {
+            return response.readEntity(ExchangeObject.class);
+        }
+        else
+        {
+            throw new ApiException(response);
+        }
     }
 
-    public ExchangeObject getExchangeObject(ExchangeObject exchangeObject) {
-        // Return null if it does not exist
-        // TODO Implement with next Exchange release
-        // String nameUrl = exchangeObject.getNameUrl();
-        // String object_path = String.format(EXCHANGE_OBJECTS_PATH_TEMPLATE, getOrgId()) + '/' + nameUrl;
+    /**
+     * Looks up an object by its model object.
+     * @param exchangeObject The object to look up.
+     * @return The object found, or null if it does not exist.
+     * @throws org.mule.tools.maven.plugin.mule.ApiException if not successful.
+     */
+    public ExchangeObject getExchangeObject(ExchangeObject exchangeObject) throws ApiException {
+        String nameUrl = exchangeObject.getNameUrl();
+        String object_path = String.format(EXCHANGE_OBJECTS_PATH_TEMPLATE, getOrgId()) + '/' + nameUrl;
+        Response response = get(uri, object_path);
 
-        return null;
+        if (response.getStatus() == 200)
+        {
+            return response.readEntity(ExchangeObject.class);
+        }
+
+        if (response.getStatus() == 404) {
+            return null;
+        }
+
+        throw new ApiException(response);
     }
 
     private String obtainExchangeToken() throws IllegalAccessException {
-        // TODO Review with next Exchange release
         String bearerToken = (String) FieldUtils.readField(this, "bearerToken", true);
         String json_string = String.format(EXCHANGE_TOKEN_REQUEST_TEMPLATE, bearerToken);
         Entity<String> json = Entity.json(json_string);
